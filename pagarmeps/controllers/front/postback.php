4b2d1ca5f6736330cdc30d678cf4dd56801624ab
Murilo Henrique <murilohns@gmail.com>
2018-03-20 17:27:08 -0300

    postback: refactor
    postback: add postback validation

--- pagarmeps/controllers/front/postback.php
+++ pagarmeps/controllers/front/postback.php
@@ -1,117 +1,123 @@
 <?php
 /**
-* 2007-2015 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    Pagar.me
-*  @copyright 2015 Pagar.me
-*  @version   1.0.0
-*  @link      https://pagar.me/
-*  @license
-*/
+ * 2007-2015 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    Pagar.me
+ *  @copyright 2015 Pagar.me
+ *  @version   1.0.0
+ *  @link      https://pagar.me/
+ *  @license
+ */
 
 class PagarmepsPostbackModuleFrontController extends ModuleFrontController
 {
-	/**
-	 * This class should be use by your Instant Payment
-	 * Notification system to validate the order remotely
-	 */
+    private function loader() {
+        include '../../lib/pagarme/PagarMe.php';
+    }
 
-	private function loader($className) {
-		//echo 'Trying to load ', $className, ' via ', __METHOD__, "()\n";
-		if(strrpos($className, 'PagarMe_') !== false) {
-			$className = Tools::substr($className, 8);
-			//echo 'Trying to load V2 ', $className, ' via ', __METHOD__, "()\n";
-			include dirname(__FILE__).'/../../lib/pagarme/'.$className . '.php';
-		}else if(strrpos($className, 'Pagarmeps') !== false) {
-			include dirname(__FILE__).'/../../classes/'.$className . '.php';
-		} else {
-			include dirname(__FILE__).'/../../lib/pagarme/'.$className . '.php';
-		}
-	}
+    public function __construct($response = array()) {
+        spl_autoload_register(array($this, 'loader'));
+        parent::__construct($response);
+        $this->display_header = false;
+        $this->display_header_javascript = false;
+        $this->display_footer = false;
+    }
 
-	public function __construct($response = array()) {
-		spl_autoload_register(array($this, 'loader'));
-		parent::__construct($response);
-		$this->display_header = false;
-		$this->display_header_javascript = false;
-		$this->display_footer = false;
-	}
+    public function postProcess()
+    {
+        if ($this->module->active == false) {
+            Pagarmeps::addLog('Postback', 1, 'info', 'Pagarme', null);
+            return header('HTTP/1.1 500 Pagarme is not active');
+        }
 
-	public function postProcess()
-	{
-		Pagarmeps::addLog('1-PostBack', 1, 'info', 'Pagarme', null);
-		/**
-		 * If the module is not active anymore, no need to process anything.
-		 */
-		if ($this->module->active == false) {
-			Pagarmeps::addLog('2-PostBack', 1, 'info', 'Pagarme', null);
-			die('This module is not active');
-		}
+        $api_key = Configuration::get('PAGARME_API_KEY');
+        Pagarme::setApiKey($api_key);
 
-		if ((Tools::isSubmit('id') == false) || (Tools::isSubmit('current_status') == false)) {
-			Pagarmeps::addLog('3-PostBack', 1, 'info', 'Pagarme', null);
-			die('No ID submited, or no Status');
-		}
+        $request_body = file_get_contents('php://input');
 
-		$id = Tools::getValue('id');
-		$current_status = Tools::getValue('current_status');
-		$current_status_id = Pagarmeps::getStatusId($current_status);
-		$transaction = Tools::getValue('transaction');
-		Pagarmeps::addLog('4-PostBack id='.$id.' | current_status='.$current_status, 1, 'info', 'Pagarme', null);
+        if(!Pagarme::validateRequestSignature($request_body, $_SERVER['HTTP_X_HUB_SIGNATURE'])){
+            Pagarmeps::addLog('Postback: dados de postback inválidos', 1, 'info', 'Pagarme', null);
+            return header('HTTP/1.1 403 Dados de postback inválidos');
+        }
 
-		$order_id = null;
-		if( isset($transaction['metadata']['cart_id']) ) {
-			$order_id = Order::getOrderByCartId($transaction['metadata']['cart_id']);
-		} else {
-			$order_id = PagarmepsTransactionClass::getOrderIdByTransactionId($id);
-		}
+        $id = Tools::getValue('id');
+        $current_status = Tools::getValue('current_status');
+        $prestashop_new_order_status = Pagarmeps::getStatusId($current_status);
+        $transaction = Tools::getValue('transaction');
 
-		Pagarmeps::addLog('5-PostBack order_id='.$order_id, 1, 'info', 'Pagarme', null);
+        Pagarmeps::addLog('Postback: transaction id='.$id.' | status:'.$current_status, 1, 'info', 'Pagarme', null);
 
-		if($order_id != null) {
-			$order = new Order($order_id);
-			if($order_id != null) {
-				$order->current_state = $current_status_id;
-				$history = new OrderHistory();
-				$history->id_order = (int)$order->id;
-				$history->id_order_state = $current_status_id;
+        $order_id = PagarmepsTransactionClass::getOrderIdByTransactionId($id);
+        if( isset($transaction['metadata']['cart_id']) ) {
+            $order_id = Order::getOrderByCartId($transaction['metadata']['cart_id']);
+        }
 
-					if($order->save()){
-						$history->addWithemail();
-						$history->changeIdOrderState($current_status_id, (int)$order->id);
-						Pagarmeps::addLog('10-PostBack: Everything is OK', 1, 'info', 'Pagarme', null);
-						die('OK');
-					} else {
-						Pagarmeps::addLog('9-PostBack: Error while saving Order', 1, 'info', 'Pagarme', null);
-						header('HTTP/1.1 500 Internal Server Error');
-						exit(0);
-					}
+        Pagarmeps::addLog('Postback: order id='.$order_id, 1, 'info', 'Pagarme', null);
 
-			} else {
-				Pagarmeps::addLog('7-PostBack: No order Object found for the saved ID', 1, 'info', 'Pagarme', null);
-				die('No order Object found for the saved ID');
-			}
-		} else {
-			Pagarmeps::addLog('6-PostBack: No saved order found for the submited ID', 1, 'info', 'Pagarme', null);
-			die('No saved order found for the submited ID');
-		}
+        $order = new Order($order_id);
 
+        if(is_null($order_id) || is_null($order)) {
+            Pagarmeps::addLog('Postback: Order not found', 1, 'info', 'Pagarme', null);
+            return header('HTTP/1.1 400 Order not found');
+        }
 
-	}
+        if(!$this->updateOrderStatus($order, $prestashop_new_order_status)) {
+            Pagarmeps::addLog('Postback: Order '. $order->id .' already ' . $current_status, 1, 'info', 'Pagarme', null);
+            return header('HTTP/1.1 200 Order already ' . $current_status);
+        }
+
+        if(!$order->hasInvoice() && $current_status == 'paid') {
+            $order->setInvoice();
+        }
+
+        $this->addOrderHistory($order, $prestashop_new_order_status);
+
+        Pagarmeps::addLog('Postback: Order ' . $order->id . ' successfully updated to' . $current_status);
+
+        return header('HTTP/1.1 200 Order successfully updated');
+    }
+
+    private function updateOrderStatus($order, $prestashop_new_order_status) {
+
+        if($order->current_state == $prestashop_new_order_status) {
+            return false;
+        }
+
+        $order->current_state = $prestashop_new_order_status;
+
+        if(!$order->save()){
+            Pagarmeps::addLog('Postback: failed to update order', 1, 'info', 'Pagarme', null);
+
+            return false;
+        }
+
+        Pagarmeps::addLog('Postback: order ' . $order->id . ' successfully updated to ' . $current_status, 1, 'info', 'Pagarme', null);
+
+        return true;
+    }
+
+    private function addOrderHistory($order, $prestashop_new_order_status) {
+        $history = new OrderHistory();
+        $history->id_order = (int)$order->id;
+        $history->id_order_state = $prestashop_new_order_status;
+
+        $history->addWithemail();
+        $history->changeIdOrderState($prestashop_new_order_status, (int)$order->id);
+    }
 }
